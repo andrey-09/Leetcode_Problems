a89cceab7b8e47399c63dca33da547a6db65c300
Andrey Vasenin <99732328+andrey-09@users.noreply.github.com>
2022-10-21 13:28:42 +0200

    Create 69_Sqrt(x).py

---
+++ 69_Sqrt(x).py
@@ -0,0 +1,21 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+        #and the try a binary search
+        
+        left, right=0, x
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if mid * mid > x:
+                right = mid - 1
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                return mid
+
+        # left > right
+        return right
+        
+        
